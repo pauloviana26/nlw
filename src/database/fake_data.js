e74d915cdceb09b7984167256e110187714786c4
pauloviana <45289241+pauloviana26@users.noreply.github.com>
2020-08-12 18:55:53 -0300

    updateDataBase

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+// Dados
+const proffys = [
+    {
+        name: "Paulo Viana",
+        avatar: "https://avatars3.githubusercontent.com/u/45289241?s=460&u=a11468cbd6bddf6cbc812553ab4bde85647fe685&v=4",
+        whatsapp: "34992008999",
+        bio: "Apaixonado por programação, trabalho atualmente com desenvolvimento web e nas horas vagas ajudo pessoas com dificuldade em iniciar na programação.",
+        subject: "Programação",
+        cost: "50",
+        weekday: [1],
+        time_from: [720] ,
+        time_to: [1220]
+    },
+    {
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "98888888",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720] ,
+        time_to: [1220]
+    },
+    {
+        name: "Maik Brito",
+        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=460&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
+        whatsapp: "3499652500",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Matemática",
+        cost: "20",
+        weekday: [0],
+        time_from: [720] ,
+        time_to: [1220]
+    }
+]
